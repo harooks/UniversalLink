474563c77373f2d7cb8135cbbcb2bd9f7a31d4e8
Haruko Okada <haruko@Haruko.local>
2019-03-03 14:25:55 -0600

    first commit

---
+++ UniversalLink/CellViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CellViewController.swift
+//  UniversalLink
+//
+//  Created by Haruko Okada on 2019/02/17.
+//  Copyright © 2019 Haruko Okada. All rights reserved.
+//
+
+import UIKit
+
+class CellViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ UniversalLink/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UniversalLink
+//
+//  Created by Haruko Okada on 2019/02/17.
+//  Copyright © 2019 Haruko Okada. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
